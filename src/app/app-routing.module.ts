39b1e4f53693dceba3a218ceb898442ce53cc155
obraia <bryan22czo@gmail.com>
2020-06-14 18:06:16 -0300

    [feature] rota dashboard adicionada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './shared/login-form/login-form.component';
 import { RegisterFormComponent } from './shared/register-form/register-form.component';
 
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: LoginFormComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
