e1ea00bcfadeaadf4dcaf47bdd4a270620695313
obraia <bryan22czo@gmail.com>
2020-06-14 18:07:40 -0300

    [feature] novos components importados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginFormComponent } from './shared/login-form/login-form.component';
 import { RegisterFormComponent } from './shared/register-form/register-form.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
